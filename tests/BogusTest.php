5712c65fc55c036e7f8bcf378e459b7517196fa0
Patrick Barsallo <patrick@coronalabs.com>
2017-04-18 16:03:48 -0500

    adding phpunit

---
+++ tests/BogusTest.php
@@ -0,0 +1,14 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class BogusTest extends TestCase
+{
+    /**
+     * Bogus unit test
+     */
+    public function testNothing()
+    {
+        $this->assertTrue(true);
+    }
+}
